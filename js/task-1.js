6e1b4b4d347cf202b626a83b8ded05ec42ad428c
Pu1cher <145374971+Pu1cher@users.noreply.github.com>
2024-01-02 16:09:32 +0200

    1

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+'use strict'; 
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice <= customerCredits) {
+        
+    }
+}
