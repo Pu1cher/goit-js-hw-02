a7acc4f99920c9134e36d81357d448a011f29ec6
Pu1cher <145374971+Pu1cher@users.noreply.github.com>
2024-01-02 19:01:58 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,14 @@
 'use strict'; 
+
+function checkForSpam(message) {
+    let messageCheck = message.toLowerCase();
+   return messageCheck.includes('sale') || messageCheck.includes('spam')
+}
+
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
